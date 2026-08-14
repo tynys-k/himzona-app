ad9e3b93a3f0b52919114ceaada206b53dd8c819
tynys-k <165224507+tynys-k@users.noreply.github.com>
2026-08-15 00:33:42 +0500

    Update supabaseClient.js

--- src/supabaseClient.js
+++ src/supabaseClient.js
@@ -2,7 +2,7 @@ import { createClient } from "@supabase/supabase-js";
 
 // Эти два значения безопасны для публикации (защита данных — на стороне базы,
 // через правила доступа, которые мы настроили на Этапе 2).
-const SUPABASE_URL = "https://tkfifsvoiqnjbjvluwub.supabase.co";
-const SUPABASE_KEY = "sb_publishable_4L8SKkHN5UNL8orN1pW6fw_6RW7v-Uq";
+const SUPABASE_URL = "https://uodytafoacsodvaacwzb.supabase.co";
+const SUPABASE_KEY = "sb_publishable_gjfqDHojhVrroGvgV84Rhg_HvY8u06j";
 
 export const supabase = createClient(SUPABASE_URL, SUPABASE_KEY);
